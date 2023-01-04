5b1bd3f03db3144a31bea01d351a262c48a2a981
alanelbailarin <031912556@ut-tijuana.edu.mx>
2023-01-04 01:02:51 -0800

    se anadio una nueva seccion de repositorios

--- src/components/projects/GithubRepos.vue
+++ src/components/projects/GithubRepos.vue
@@ -2,7 +2,35 @@
   <div
     class="dark:bg-gray-700 p-3 rounded-md shadow-lg bg-white dark:border-none border border-gray-200"
   >
-    a
+    <div class="md:grid grid-cols-2 gap-4">
+      <div v-for="item in dataRepos" :key="dataRepos.id" class="mb-3">
+        <div class="flex justify-between items-center">
+          <a
+            :href="item.html_url"
+            target="_blank"
+            class="text-gray-700 font-semibold dark:text-white text-sm hover:underline"
+            >{{ item.name }}</a
+          >
+          <span
+            class="text-green-600 dark:bg-green-200 bg-green-100 text-xs p-1 font-semibold rounded-md"
+            v-if="item.private === false"
+            >Publico</span
+          >
+          <span
+            class="text-red-600 dark:bg-red-200 bg-red-100 text-xs p-1 font-semibold rounded-md"
+            v-else
+            >Privado</span
+          >
+        </div>
+        <p class="text-sm text-gray-400 mt-1" v-if="item.description">
+          {{ item.description }}
+        </p>
+        <p class="text-sm text-gray-400 mt-1" v-else>
+          No hay una descripción aun.
+        </p>
+        <time class="mt-2 text-xs text-gray-400">{{ item.created_at }}</time>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -11,18 +39,16 @@ export default {
 };
 </script>
 <script setup lang="ts">
-import { onMounted, ref, nextTick } from "vue";
-import { getRepos } from "@/api/githubApi";
+import { ref } from "vue";
+import type { githubRepos } from "@/types/githubRepos";
 
-const dataRepos = ref({});
+const dataRepos = ref<githubRepos>({} as githubRepos);
 
-const getUserRepos = () => {
-  nextTick(async () => {
-    const dataRepos = await getRepos();
+fetch("https://api.github.com/users/alansinbailarin/repos")
+  .then((res) => res.json())
+  .then((data) => {
+    dataRepos.value = data;
+
+    console.log(dataRepos.value);
   });
-};
-
-onMounted(() => {
-  getUserRepos();
-});
 </script>
